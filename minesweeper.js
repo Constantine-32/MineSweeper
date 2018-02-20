e3d02b0f9ad9feb2f0d5ceb2edd889b612ab70af
Christian C. Romero <id.32@outlook.com>
2018-02-21 00:17:16 +0100

    translated js code from ES5 to ES6

--- minesweeper.js
+++ minesweeper.js
@@ -1,57 +1,69 @@
-function Minesweeper(options) {
-  var NEWG = true
-  var ROWS = options.numRows
-  var COLS = options.numCols
-  var MINE = options.numMines
-  var GAME = []
-  var HSID = ""
+'use strict';
 
-  ;(function() {
-    var gamediv = document.getElementById("game");
-    gamediv.style.height = ROWS * 16 + "px"
-    gamediv.style.width = COLS * 16 + "px"
-    for (var row = 1; row <= ROWS; row++) for (var col = 1; col <= COLS; col++) {
-      var squarediv = document.createElement("div")
-      squarediv.className = "square blank"
-      squarediv.id = row+"_"+col
-      gamediv.appendChild(squarediv)
-    }
-  }) ()
+/**
+ * Minesweeper.js
+ * Author: Christian C. Romero
+ * Date: February 2018
+ * Version: 1.0
+ */
 
-  document.addEventListener('mousedown', function (e) {
-    if (e.button === 0 && HSID) {
-      console.log("Left Click Down on: "+HSID)
-    }
-    if (e.button === 2 && HSID) {
-      console.log("Right Click Down on: "+HSID)
-    }
-  })
-  document.addEventListener('mouseup', function (e) {
-    if (e.button === 0 && HSID) {
-      console.log("Left Click Up on: "+HSID)
-    }
-  })
-  document.addEventListener('keydown', function (e) {
-    if (e.which === 32 && HSID) {
-      console.log("Space Key Down on: "+HSID)
-    }
-  })
-  document.addEventListener('mouseover', function (e) {
-    if (isSquare(e.target)) {
-      HSID = e.target.id
-    }
-  })
-  document.addEventListener('mouseout', function (e) {
-    if (isSquare(e.target)) {
-      if (HSID = e.target.id) {
-        HSID = ""
+class Minesweeper {
+  constructor(options) {
+    this.NEWG = true
+    this.ROWS = options.numRows
+    this.COLS = options.numCols
+    this.MINE = options.numMines
+    this.GAME = []
+
+    let gamediv = document.getElementById("game");
+    gamediv.style.height = this.ROWS * 16 + "px"
+    gamediv.style.width = this.COLS * 16 + "px"
+    for (let row = 1; row <= this.ROWS; row++) {
+      for (let col = 1; col <= this.COLS; col++) {
+        let squarediv = document.createElement("div")
+        squarediv.className = "square blank"
+        squarediv.id = row + "_" + col
+        gamediv.appendChild(squarediv)
       }
     }
-  })
-
-  function isSquare(element) {
-    return element.className.substring(0, 6) === "square"
   }
 }
 
-Minesweeper({gameTypeId: 3, numRows: 16, numCols: 30, numMines: 99});
+new Minesweeper({gameTypeId: 3, numRows: 16, numCols: 30, numMines: 99});
+
+let HSID = ''
+
+function isSquare(element) {
+  return element.className.substring(0, 6) === "square"
+}
+
+document.addEventListener('mousedown', function (e) {
+  if (e.button === 0 && HSID) {
+    console.log("Left Click Down on: " + HSID)
+  }
+  if (e.button === 2 && HSID) {
+    console.log("Right Click Down on: " + HSID)
+  }
+})
+document.addEventListener('mouseup', function (e) {
+  if (e.button === 0 && HSID) {
+    console.log("Left Click Up on: " + HSID)
+  }
+})
+document.addEventListener('keydown', function (e) {
+  if (e.which === 32 && HSID) {
+    console.log("Space Key Down on: " + HSID)
+  }
+})
+document.addEventListener('mouseover', function (e) {
+  if (isSquare(e.target)) {
+    HSID = e.target.id
+  }
+})
+document.addEventListener('mouseout', function (e) {
+  if (isSquare(e.target)) {
+    if (HSID = e.target.id) {
+      HSID = ""
+    }
+  }
+})
